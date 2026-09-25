ff7d998c4f2addf33e4723cc5670c3738d5485ee
Habeebu Rahman CA <habeebu@seeroo.com>
2026-09-25 16:07:32 +0530

    feat: new pwa icons added

--- apps/web/src/app/manifest.ts
+++ apps/web/src/app/manifest.ts
@@ -14,25 +14,25 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ['fitness', 'health', 'sports', 'lifestyle'],
     icons: [
       {
-        src: '/icon-192.png',
+        src: '/icons/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/icon-192.png',
+        src: '/icons/icon-maskable-192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/icon-512.png',
+        src: '/icons/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/icon-512.png',
+        src: '/icons/icon-maskable-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
